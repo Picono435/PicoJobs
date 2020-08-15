ef460ede73f819d917debb238d51bbca24e116ee
Picono435 <57836766+Picono435@users.noreply.github.com>
2020-08-15 15:54:44 +0100

    Added MySQL storage method

--- src/main/java/com/gmail/picono435/picojobs/managers/StorageManager.java
+++ src/main/java/com/gmail/picono435/picojobs/managers/StorageManager.java
@@ -3,6 +3,7 @@ package com.gmail.picono435.picojobs.managers;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
@@ -12,6 +13,7 @@ import com.gmail.picono435.picojobs.api.JobPlayer;
 import com.gmail.picono435.picojobs.api.PicoJobsAPI;
 import com.gmail.picono435.picojobs.api.StorageMethod;
 import com.gmail.picono435.picojobs.utils.FileCreator;
+import com.gmail.picono435.picojobs.utils.MySQLAPI;
 
 public class StorageManager {
 	
@@ -19,14 +21,31 @@ public class StorageManager {
 	
 	public void getData() {
 		storageMethod = StorageMethod.getStorageMethod(PicoJobsPlugin.getPlugin().getConfig().getConfigurationSection("storage").getString("storage-method"));
+		
 		if(storageMethod == StorageMethod.YAML) {
+			PicoJobsPlugin.sendConsoleMessage(ChatColor.AQUA + "[PicoJobs] Using the YAML storage method.");
 			getDataInConfig();
 			return;
 		}
+		
+		if(storageMethod == StorageMethod.MYSQL) {
+			PicoJobsPlugin.sendConsoleMessage(ChatColor.AQUA + "[PicoJobs] Using the MySQL storage method.");
+			getDataInMySQL();
+			return;
+		}
+		
+		PicoJobsPlugin.sendConsoleMessage(ChatColor.YELLOW + "[PicoJobs] We did not find any storage method with that name. Using YAML storage method as default.");
+		getDataInConfig();
 	}
 	
 	private void getDataInMySQL() {
-		
+		MySQLAPI api = new MySQLAPI();
+		api.startConnection();
+		for(String uuid : api.getAllUsers()) {
+			System.out.println(uuid);
+			PicoJobsPlugin.playersdata.put(UUID.fromString(uuid), api.getFromDB(uuid));
+		}
+		api.close();
 	}
 	
 	private void getDataInConfig() {
@@ -47,18 +66,33 @@ public class StorageManager {
 	}
 	
 	public void saveData() {
+		storageMethod = StorageMethod.getStorageMethod(PicoJobsPlugin.getPlugin().getConfig().getConfigurationSection("storage").getString("storage-method"));
+		
 		if(storageMethod == StorageMethod.YAML) {
+			PicoJobsPlugin.sendConsoleMessage(ChatColor.AQUA + "[PicoJobs] Using the YAML storage method.");
 			saveInConfig();
 			return;
 		}
+		
 		if(storageMethod == StorageMethod.MYSQL) {
-			
+			PicoJobsPlugin.sendConsoleMessage(ChatColor.AQUA + "[PicoJobs] Using the MySQL storage method.");
+			saveInMySQL();
 			return;
 		}
+		
+		PicoJobsPlugin.sendConsoleMessage(ChatColor.YELLOW + "[PicoJobs] We did not find any storage method with that name. Using YAML storage method as default.");
+		saveInConfig();
 	}
 	
 	private void saveInMySQL() {
-		
+		MySQLAPI api = new MySQLAPI();
+		api.startConnection();
+		api.deleteMysqlRecords();
+		for(UUID uuid : PicoJobsPlugin.playersdata.keySet()) {
+			JobPlayer jp = PicoJobsPlugin.playersdata.get(uuid);
+			api.addINDB(uuid.toString(), jp.getJob().getName(), jp.getMethod(), jp.getMethodLevel(), jp.getSalary(), jp.isWorking());
+		}
+		api.close();
 	}
 	
 	private void saveInConfig() {
