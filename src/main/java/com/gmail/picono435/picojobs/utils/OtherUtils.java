53e1a51dbdee74ed3c026d031054dbfbd155ef3a
Picono435 <57836766+Picono435@users.noreply.github.com>
2020-12-28 10:23:31 +0000

    Added ability to use minecraft: in entity searches

--- src/main/java/com/gmail/picono435/picojobs/utils/OtherUtils.java
+++ src/main/java/com/gmail/picono435/picojobs/utils/OtherUtils.java
@@ -7,6 +7,9 @@ public class OtherUtils {
 	public static EntityType getEntityByName(String name) {
 		name = name.toLowerCase();
         for (EntityType type : EntityType.values()) {
+        	if(name.toLowerCase().startsWith("minecraft:")) {
+            	name = name.toLowerCase().replace("minecraft:", "");
+            }
             if(type.name().equalsIgnoreCase(name)) {
                 return type;
             }
