dea5c925f5c4a6e2876aae60532750afa1f1dac6
Picono435 <57836766+Picono435@users.noreply.github.com>
2021-05-10 15:13:20 +0100

    Fixed issue where table was not getting created with SQLite and H2

--- src/main/java/com/gmail/picono435/picojobs/storage/sql/file/H2Storage.java
+++ src/main/java/com/gmail/picono435/picojobs/storage/sql/file/H2Storage.java
@@ -2,6 +2,7 @@ package com.gmail.picono435.picojobs.storage.sql.file;
 
 import java.lang.reflect.Constructor;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 import com.gmail.picono435.picojobs.PicoJobsPlugin;
@@ -12,6 +13,11 @@ public class H2Storage extends SqlStorageFactory {
 	protected boolean initializeStorage() throws Exception {		
 		Constructor<?> constructor = Class.forName("org.h2.jdbc.JdbcConnection").getConstructor(String.class, Properties.class);
 		this.conn = (Connection) constructor.newInstance("jbdc:h2:" + PicoJobsPlugin.getInstance().getDataFolder().getPath().toString(), new Properties());
+		
+		PreparedStatement stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS ? (`uuid` VARCHAR(255) NOT NULL, `job` TEXT, `method` DOUBLE DEFAULT '0', `level` DOUBLE DEFAULT '0', `salary` DOUBLE DEFAULT '0', `is-working` BOOLEAN, PRIMARY KEY (`uuid`));");
+    	stm.setString(1, configurationSection.getString("tablename"));
+    	stm.execute();
+    	stm.close();
 		return true;
 	}
 
--- src/main/java/com/gmail/picono435/picojobs/storage/sql/file/SqliteStorage.java
+++ src/main/java/com/gmail/picono435/picojobs/storage/sql/file/SqliteStorage.java
@@ -2,6 +2,7 @@ package com.gmail.picono435.picojobs.storage.sql.file;
 
 import java.lang.reflect.Constructor;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 import com.gmail.picono435.picojobs.PicoJobsPlugin;
@@ -12,6 +13,11 @@ public class SqliteStorage extends SqlStorageFactory {
 	protected boolean initializeStorage() throws Exception {		
 		Constructor<?> constructor = Class.forName("org.sqlite.jdbc4.JDBC4Connection").getConstructor(String.class, String.class, Properties.class);
 		this.conn = (Connection) constructor.newInstance("jdbc:sqlite:" + PicoJobsPlugin.getInstance().getDataFolder().getPath().toString(), PicoJobsPlugin.getInstance().getDataFolder().getPath().toString(), new Properties());
+		
+		PreparedStatement stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS ? (`uuid` VARCHAR(255) NOT NULL, `job` TEXT, `method` DOUBLE DEFAULT '0', `level` DOUBLE DEFAULT '0', `salary` DOUBLE DEFAULT '0', `is-working` BOOLEAN, PRIMARY KEY (`uuid`));");
+    	stm.setString(1, configurationSection.getString("tablename"));
+    	stm.execute();
+    	stm.close();
 		return true;
 	}
 
