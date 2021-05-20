6f7cb34e76ca6140d3b256fa38a56b46b7a359c9
Picono435 <57836766+Picono435@users.noreply.github.com>
2021-05-20 20:20:13 +0100

    Fixed small issue with HOCON, JSON and YAML storage methods

--- src/main/java/com/gmail/picono435/picojobs/storage/file/ConfigurationStorageFactory.java
+++ src/main/java/com/gmail/picono435/picojobs/storage/file/ConfigurationStorageFactory.java
@@ -23,75 +23,75 @@ public abstract class ConfigurationStorageFactory extends StorageFactory {
 	
 	@Override
 	public boolean createPlayer(UUID uuid) throws Exception {
-		this.root.getNode(uuid, "method").setValue(0D);
-		this.root.getNode(uuid, "level").setValue(0D);
-		this.root.getNode(uuid, "salary").setValue(0D);
-		this.root.getNode(uuid, "is-working").setValue(false);
+		this.root.getNode(uuid.toString(), "method").setValue(0D);
+		this.root.getNode(uuid.toString(), "level").setValue(0D);
+		this.root.getNode(uuid.toString(), "salary").setValue(0D);
+		this.root.getNode(uuid.toString(), "is-working").setValue(false);
 		this.loader.save(root);
 		return true;
 	}
 
 	@Override
 	public boolean playerExists(UUID uuid) throws Exception {
-		return !this.root.getNode(uuid).isVirtual();
+		return !this.root.getNode(uuid.toString()).isVirtual();
 	}
 
 	@Override
 	public String getJob(UUID uuid) throws Exception {
-		return this.root.getNode(uuid, "job").getString();
+		return this.root.getNode(uuid.toString(), "job").getString();
 	}
 
 	@Override
 	public double getMethod(UUID uuid) throws Exception {
-		return this.root.getNode(uuid, "method").getDouble();
+		return this.root.getNode(uuid.toString(), "method").getDouble();
 	}
 
 	@Override
 	public double getMethodLevel(UUID uuid) throws Exception {
-		return this.root.getNode(uuid, "level").getDouble();
+		return this.root.getNode(uuid.toString(), "level").getDouble();
 	}
 
 	@Override
 	public boolean isWorking(UUID uuid) throws Exception {
-		return this.root.getNode(uuid, "is-working").getBoolean();
+		return this.root.getNode(uuid.toString(), "is-working").getBoolean();
 	}
 
 	@Override
 	public double getSalary(UUID uuid) throws Exception {
-		return this.root.getNode(uuid, "salary").getDouble();
+		return this.root.getNode(uuid.toString(), "salary").getDouble();
 	}
 
 	@Override
 	public boolean setJob(UUID uuid, String job) throws Exception {
-		this.root.getNode(uuid, "job").setValue(job);
+		this.root.getNode(uuid.toString(), "job").setValue(job);
 		this.loader.save(this.root);
 		return true;
 	}
 
 	@Override
 	public boolean setMethod(UUID uuid, double method) throws Exception {
-		this.root.getNode(uuid, "method").setValue(method);
+		this.root.getNode(uuid.toString(), "method").setValue(method);
 		this.loader.save(this.root);
 		return true;
 	}
 
 	@Override
 	public boolean setMethodLevel(UUID uuid, double level) throws Exception {
-		this.root.getNode(uuid, "level").setValue(level);
+		this.root.getNode(uuid.toString(), "level").setValue(level);
 		this.loader.save(this.root);
 		return true;
 	}
 
 	@Override
 	public boolean setWorking(UUID uuid, boolean isWorking) throws Exception {
-		this.root.getNode(uuid, "is-working").setValue(isWorking);
+		this.root.getNode(uuid.toString(), "is-working").setValue(isWorking);
 		this.loader.save(this.root);
 		return true;
 	}
 
 	@Override
 	public boolean setSalary(UUID uuid, double salary) throws Exception {
-		this.root.getNode(uuid, "salary").setValue(salary);
+		this.root.getNode(uuid.toString(), "salary").setValue(salary);
 		this.loader.save(this.root);
 		return true;
 	}
