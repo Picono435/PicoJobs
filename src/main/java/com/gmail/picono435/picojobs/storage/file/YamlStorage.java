c29755902678b97691147f5834873cea2bfea675
Picono435 <57836766+Picono435@users.noreply.github.com>
2021-05-30 10:22:41 +0100

    Added backward compatibility

--- src/main/java/com/gmail/picono435/picojobs/storage/file/YamlStorage.java
+++ src/main/java/com/gmail/picono435/picojobs/storage/file/YamlStorage.java
@@ -1,5 +1,10 @@
 package com.gmail.picono435.picojobs.storage.file;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.loader.ConfigurationLoader;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
@@ -10,6 +15,25 @@ public class YamlStorage extends ConfigurationStorageFactory {
 
 	@Override
 	protected ConfigurationLoader<? extends ConfigurationNode> loadFile() {
+		File oldData = new File(PicoJobsPlugin.getInstance().getDataFolder().toPath().toAbsolutePath().resolve("data.yml").toString());
+		if(oldData.exists()) {
+			try {
+				String content = new String(Files.readAllBytes(oldData.toPath()));
+				content = content.replace("playerdata:", "");
+				content = content.replace("/^  /mg", "");
+				oldData.delete();
+				File newData = new File(PicoJobsPlugin.getInstance().getDataFolder().toPath().toAbsolutePath()
+						.resolve("storage").resolve("picojobs.yml").toString());
+				newData.getParentFile().mkdirs();
+				newData.createNewFile();
+				FileWriter myWriter = new FileWriter(newData);
+				myWriter.write(content);
+				myWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		return YamlConfigurationLoader.builder()
         .indent(2)
         .path(PicoJobsPlugin.getInstance().getDataFolder().toPath().toAbsolutePath()
