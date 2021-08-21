b0c4350f0e8086b7c2d74fd73fab72542108d41d
Picono435 <57836766+Picono435@users.noreply.github.com>
2021-08-21 21:27:23 +0100

    Revert "Update to 1.0-beta-3d"

--- src/main/java/com/gmail/picono435/picojobs/listeners/jobs/FillListener.java
+++ src/main/java/com/gmail/picono435/picojobs/listeners/jobs/FillListener.java
@@ -19,11 +19,12 @@ public class FillListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onFillBucket(PlayerBucketFillEvent  e) {
+		if(e.getPlayer() == null) return;
 		Block b = e.getBlockClicked();
-		if(PicoJobsPlugin.getInstance().isNewerThan("1.17")) {
-			if(!b.isLiquid() && b.getType() == Material.POWDER_SNOW) return;
+		if(!PicoJobsPlugin.getInstance().isNewerThan("1.17")) {
+			if(b == null || (!b.isLiquid())) return;
 		} else {
-			if(!b.isLiquid()) return;
+			if(b == null || (!b.isLiquid() && b.getType() == Material.POWDER_SNOW)) return;
 		}
 		Player p = e.getPlayer();
 		JobPlayer jp = PicoJobsAPI.getPlayersManager().getJobPlayer(p);
