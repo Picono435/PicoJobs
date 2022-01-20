55aea77eb9e641c671a6dd46e99c916fb6c0bd41
Picono435 <picono435@gmail.com>
2022-01-20 17:20:34 +0000

    Fixed issue with seeds not being detected (#144)

--- src/main/java/com/gmail/picono435/picojobs/listeners/jobs/BreakListener.java
+++ src/main/java/com/gmail/picono435/picojobs/listeners/jobs/BreakListener.java
@@ -1,11 +1,15 @@
 package com.gmail.picono435.picojobs.listeners.jobs;
 
+import org.bukkit.CropState;
+import org.bukkit.Material;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.material.Crops;
 import org.bukkit.metadata.FixedMetadataValue;
 
 import com.gmail.picono435.picojobs.PicoJobsPlugin;
@@ -20,16 +24,28 @@ public class BreakListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onBreakBlock(BlockBreakEvent e) {
 		if(e.getBlock().getMetadata("PLACED").size() > 0) return;
-		
+
 		Player p = e.getPlayer();
 		JobPlayer jp = PicoJobsAPI.getPlayersManager().getJobPlayer(p);
 		if(!jp.hasJob()) return;
 		if(!jp.isWorking()) return;
 		Job job = jp.getJob();
 		if(!job.getTypes().contains(Type.BREAK)) return;
-		
+		System.out.println(e.getBlock().getType().name());
+		if(PicoJobsPlugin.getInstance().isOlderThan("1.12.2")) {
+			if(e.getBlock().getState().getData() instanceof Crops) {
+				if(((Crops)e.getBlock().getState().getData()).getState() == CropState.RIPE) return;
+			}
+		} else {
+			if(e.getBlock().getBlockData() instanceof Ageable) {
+				Ageable ageable = (Ageable) e.getBlock().getBlockData();
+				System.out.println(ageable.getAge() + " " + ageable.getMaximumAge());
+				if(ageable.getMaximumAge() != ageable.getAge()) return;
+			}
+		}
+
 		if(!job.inWhitelist(e.getBlock().getType())) return;
-		
+
 		if(jp.simulateEvent()) {
 			p.sendMessage(LanguageManager.getMessage("finished-work", p));
 		}
@@ -37,7 +53,13 @@ public class BreakListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlaceBlock(BlockPlaceEvent e) {
-		if(e.getPlayer() == null) return;
+		if(e.getBlock().getType() == Material.BEETROOT_SEEDS ||
+				e.getBlock().getType() == Material.MELON_SEEDS ||
+				e.getBlock().getType() == Material.PUMPKIN_SEEDS ||
+				e.getBlock().getType() == Material.WHEAT_SEEDS ||
+				e.getBlock().getType() == Material.CARROT ||
+				e.getBlock().getType() == Material.POTATO
+		) return;
 		e.getBlock().setMetadata("PLACED", new FixedMetadataValue(PicoJobsPlugin.getInstance(), "YES"));
 	}
 }
