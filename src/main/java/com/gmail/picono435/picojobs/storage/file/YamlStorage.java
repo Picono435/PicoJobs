66e88b2bef709a200430cc31a17feff63cde0482
Picono435 <picono435@gmail.com>
2023-03-09 18:20:18 +0000

    Removed old yaml config storage migration as it is no longer needed

--- src/main/java/com/gmail/picono435/picojobs/storage/file/YamlStorage.java
+++ src/main/java/com/gmail/picono435/picojobs/storage/file/YamlStorage.java
@@ -15,25 +15,6 @@ public class YamlStorage extends ConfigurationStorageFactory {
 
 	@Override
 	protected ConfigurationLoader<? extends ConfigurationNode> loadFile() {
-		File oldData = new File(PicoJobsPlugin.getInstance().getDataFolder().toPath().toAbsolutePath().resolve("data.yml").toString());
-		if(oldData.exists()) {
-			try {
-				String content = new String(Files.readAllBytes(oldData.toPath()));
-				content = content.replace("playerdata:", "");
-				content = content.replace("/^  /mg", "");
-				oldData.delete();
-				File newData = new File(PicoJobsPlugin.getInstance().getDataFolder().toPath().toAbsolutePath()
-						.resolve("storage").resolve("picojobs.yml").toString());
-				newData.getParentFile().mkdirs();
-				newData.createNewFile();
-				FileWriter myWriter = new FileWriter(newData);
-				myWriter.write(content);
-				myWriter.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
 		return YamlConfigurationLoader.builder()
         .indent(2)
         .path(PicoJobsPlugin.getInstance().getDataFolder().toPath().toAbsolutePath()
