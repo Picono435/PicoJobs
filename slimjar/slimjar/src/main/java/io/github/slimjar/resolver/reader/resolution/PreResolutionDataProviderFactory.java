00652cc2a60df6cc3a22eed2ed36c823b0d0ed08
Picono435 <picono435@gmail.com>
2023-05-08 14:54:41 +0100

    Removed slimjar from the repository and added maven publisher

--- slimjar/slimjar/src/main/java/io/github/slimjar/resolver/reader/resolution/PreResolutionDataProviderFactory.java
+++
@@ -1,9 +0,0 @@
-package io.github.slimjar.resolver.reader.resolution;
-
-
-import java.net.URL;
-
-@FunctionalInterface
-public interface PreResolutionDataProviderFactory {
-    PreResolutionDataProvider create(final URL resolutionFileURL);
-}
