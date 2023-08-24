33feb085d07265336975651e454e092b1f1a92a6
Picono435 <picono435@gmail.com>
2023-08-24 13:45:07 +0100

    Registered ITEM, COMMAND, GRANDECONOMY & EXP as economy implementations for forge & fabric

--- common-mod/src/main/java/com/gmail/picono435/picojobs/mod/platform/ModSoftwareHooker.java
+++ common-mod/src/main/java/com/gmail/picono435/picojobs/mod/platform/ModSoftwareHooker.java
@@ -1,12 +1,21 @@
 package com.gmail.picono435.picojobs.mod.platform;
 
+import com.gmail.picono435.picojobs.api.PicoJobsAPI;
 import com.gmail.picono435.picojobs.common.platform.SoftwareHooker;
+import com.gmail.picono435.picojobs.mod.hooks.economy.CommandImplementation;
+import com.gmail.picono435.picojobs.mod.hooks.economy.ExpImplementation;
+import com.gmail.picono435.picojobs.mod.hooks.economy.GrandEconomyImplementation;
+import com.gmail.picono435.picojobs.mod.hooks.economy.ItemImplementation;
 
 public class ModSoftwareHooker implements SoftwareHooker {
     @Override
     public void hookInPhase(Phase phase) {
         switch(phase) {
             case ONE -> {
+                PicoJobsAPI.registerEconomy(new ExpImplementation());
+                PicoJobsAPI.registerEconomy(new CommandImplementation());
+                PicoJobsAPI.registerEconomy(new ItemImplementation());
+                PicoJobsAPI.registerEconomy(new GrandEconomyImplementation());
             }
             case TWO -> {
             }
