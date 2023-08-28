3ea1579a9e62c24e6638335d487def8357eacc3e
Picono435 <picono435@gmail.com>
2023-08-28 11:24:37 +0100

    Delted resolved TODOs

--- common/src/main/java/com/gmail/picono435/picojobs/common/platform/WhitelistConverter.java
+++ common/src/main/java/com/gmail/picono435/picojobs/common/platform/WhitelistConverter.java
@@ -4,7 +4,6 @@ import com.gmail.picono435.picojobs.api.Type;
 
 import java.util.List;
 
-//TODO: Add a separation between block whitelist types and item whitelist types
 public interface WhitelistConverter {
 
     boolean inStringList(Object object, Type type, List<String> whitelist);
