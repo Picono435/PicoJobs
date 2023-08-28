542f631e397777f4c8b6e36a2b150e71a30377ab
Picono435 <picono435@gmail.com>
2023-08-28 11:37:56 +0100

    Added support for arguments in aliases

--- common/src/main/java/com/gmail/picono435/picojobs/common/listeners/AliasesListener.java
+++ common/src/main/java/com/gmail/picono435/picojobs/common/listeners/AliasesListener.java
@@ -3,20 +3,24 @@ package com.gmail.picono435.picojobs.common.listeners;
 import com.gmail.picono435.picojobs.api.managers.LanguageManager;
 import com.gmail.picono435.picojobs.common.command.api.Sender;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AliasesListener {
 
-    //TODO: This is wrong lol? No arguments are being allowed
     public static boolean onCommand(Sender sender, String message) {
         String cmd = message.split(" ")[0];
         if(!cmd.startsWith("/")) cmd = "/" + cmd;
         if(cmd.equals("/jobs")) return false;
         if(cmd.equals("/jobsadmin")) return false;
+        List<String> args = Arrays.asList(message.split(" "));
+        args.remove(0);
         if(LanguageManager.getCommandAliases("jobs").contains(cmd)) {
-            sender.dispatchCommand("jobs");
+            sender.dispatchCommand("jobs " + String.join(" ", args));
             return true;
         }
         if(LanguageManager.getCommandAliases("jobsadmin").contains(cmd)) {
-            sender.dispatchCommand("jobsadmin");
+            sender.dispatchCommand("jobsadmin " + String.join(" ", args));
             return true;
         }
         return false;
